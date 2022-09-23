b45b90b1078523c7391bf481c11f7dc0e095ab66
MichGM <110921306+MichGM@users.noreply.github.com>
2022-09-23 14:32:09 -0500

    Actualizando documentos despliegue HEROKU

--- C3P_Be/c3pProject/settings.py
+++ C3P_Be/c3pProject/settings.py
@@ -146,3 +146,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+# import django_heroku
+# django_heroku.settings(locals())
