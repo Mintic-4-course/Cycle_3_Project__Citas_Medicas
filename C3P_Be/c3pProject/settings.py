5bc56d6092c9d6ca59adfab539b18f8546dd26b4
MichGM <110921306+MichGM@users.noreply.github.com>
2022-09-23 19:16:24 -0500

    Despliegue OK,

--- C3P_Be/c3pProject/settings.py
+++ C3P_Be/c3pProject/settings.py
@@ -147,6 +147,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# import os
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
